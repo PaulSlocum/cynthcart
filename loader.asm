aaaa6336df5c4265980e2b44cabd0177f737c52b
Paul Slocum <paul.slocum@gmail.com>
2016-05-05 14:41:48 -0700

    Got under 8k compressed version working as PRG

--- loader.asm
+++ loader.asm
@@ -30,15 +30,17 @@
 
 	; Image run mode:
 CART equ 0 ; run at $8000 off cartridge ROM
-DISK equ 1 ; run at $8000, include initial load location word
+DISK equ 1 ; run at $2047, include BASIC sys command
+RAM equ 2 ; run at $8000 in RAM from PRG 
 
 ;**********************************************************
 ;**********************************************************
 ; PROGRAM CONFIGURATION SWITCHES
 ;**********************************************************
 ;**********************************************************
-MODE equ DISK   ; DISK mode is for testing
+;MODE equ DISK   ; DISK mode is for testing
 ;MODE equ CART
+MODE equ RAM
 
 ;**********************************************************
 ;**********************************************************
@@ -73,6 +75,17 @@ BASEADDR equ 2047 ; 2047 = $7FF
 	; next effective address after this is 2061 / $80D
 	ENDIF
 
+	;==================================================
+	; load from disk as PRG with auto-run
+	IF MODE=RAM
+BASEADDR equ $8000 ;
+	org BASEADDR-2 ; the beginning of the BASIC program area
+	; disk load location
+	byte $00,$80
+	ENDIF
+
+
+	
 	;---------------------------------------
 	; variables and constants here
 	;---------------------------------------
@@ -95,29 +108,96 @@ Startup:
 	cli
 	ENDIF
 
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
-	nop
+;	nop
+;	nop
+;	nop
+;	nop
+;	nop
+;	nop
+;	nop
+;	nop
+;	nop
+;	nop
 	
-	; COPY DECOMPRESSOR INTO MEMORY
+FROM equ 34
+TO equ 36
+SIZEL equ 150
+SIZEH equ 151
+	
+; Move memory down
+;
+; FROM = source start address
+;   TO = destination start address
+; SIZE = number of bytes to move
+;
+
+	IF MODE=999
+	ldy #<compressedData ;L
+	ldx #>compressedData ;H
+	sty FROM+0
+	stx FROM+1
+	ldy #$01 ; L $801 start of BASIC area
+	ldx #$08 ; H $801 start of BASIC area
+	sty TO+0
+	stx TO+1
+	ldy #$00 ; L 
+	ldx #$20 ; H 8K bytes
+	sty TO+0
+	stx TO+1
+	
+	lda #23
+	sta 1066
+
+MOVEDOWN LDY #0
+         LDX SIZEH
+         BEQ MD2
+MD1      LDA (FROM),Y ; move a page at a time
+         STA (TO),Y
+         INY
+         BNE MD1
+         INC FROM+1
+         INC TO+1
+         DEX
+         BNE MD1
+MD2      LDX SIZEL
+         BEQ MD4
+MD3      LDA (FROM),Y ; move the remaining bytes
+         STA (TO),Y
+         INY
+         DEX
+         BNE MD3
+MD4      ;RTS
+
+	ENDIF
+
+	;lda #23
+	;sta 1066
+	
+	;jmp 2061 ; JMP to start of built-in decomopression routine
+	;rts
+	
+;lock2: jmp lock2
+
+		; COPY DECOMPRESSOR INTO MEMORY
 	ldx #0
+	ldy #32
 decompCopyLoop:
-	lda decompressor,x
-	sta $C000,x
-	lda decompressor+256,x
-	sta $C000+256,x
-	lda #0
-	sta 1024+40,x
-	sta 1024+40+256,x
+	lda compressedData+2,x
+storeLocation:
+	sta $0801,x
 	dex
 	bne decompCopyLoop
+	inc decompCopyLoop+2
+	inc storeLocation+2
+	dey
+	bne decompCopyLoop
+	
+	;lda #0
+	;sta 1068
+
+	;rts	
+;lock: jmp lock
+	jmp 2061
 	
 	
 	
@@ -135,26 +215,28 @@ decompCopyLoop:
 	sta 1024
 	lda #1 ; DEBUG put character on screen
 	sta 1025
-	lda #2 ; DEBUG put character on screen
-	sta 1026
+	;lda #2 ; DEBUG put character on screen
+	;sta 1026
 
-	ldx #>compressedData ;H
-	ldy #<compressedData ;L
-	iny
-	iny
-	sty 1028
-	jsr $C000
+	jmp 2061 ; JMP to start of built-in decomopression routine
 	
-	bcc good
+	;ldx #>compressedData ;H
+	;ldy #<compressedData ;L
+	;iny
+	;iny
+	;sty 1028
+	;jsr $C000
+	
+	;bcc good
 bad:
-	lda #2 ; 'b'
-	sta 1030
-	jmp stop
+	;lda #2 ; 'b'
+	;sta 1030
+	;jmp stop
 good:
-	lda #7 ; 'g'
-	sta 1028
+	;lda #7 ; 'g'
+	;sta 1028
 	
-	jmp $5000
+	;jmp $5000
 ;	ORG $c000
 
 	; Call with X = HI of packed data, Y = LO of packed data
@@ -173,7 +255,7 @@ compressedData:
 	;incbin "eprom.cmp" ; DEBUG!!
 
 decompressor:
-	include "sa_uncrunch.asm"
+	;include "sa_uncrunch.asm"
 
 		
 	;IF MODE=KERNEL_OBSOLETE
